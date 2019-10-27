47070bbb32075e45468e5e3337c102601349ad99
maxastuart <maxastuart@gmail.com>
2019-10-27 22:50:39 +0000

    0 Bubble Sort & Swap

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "sort.h"
+
 
 /**
  * swap_int - swaps the values of two integers
@@ -15,8 +17,29 @@ void swap_int(int *a, int *b)
 	*b = a1;
 }
 
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: pointer to an array of integers
+ * @size: the number of elements in the array
+ */
 void bubble_sort(int *array, size_t size)
 {
+	int swapped = 1;
+	size_t i, x = size;
 
+	while (swapped == 1)
+	{
+		swapped = 0;
+		for (i = 1; i < x; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				swap_int(&array[i - 1], &array[i]);
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		x--;
+	}
 
 }
