0d3e104037eabc4a7c0cefa41f6b75c9bab17f5d
zinczar <brendan@wineisneat.com>
2019-10-28 21:14:50 +0000

    Not working #1 code

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,22 +7,39 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp = list->next, *temp2;
+	listint_t *temp2, *temp = (*list)->next;
+
+        if (list == NULL)
+                return;
 
 	while (temp != NULL)
 	{
-		if(temp->prev->n > temp->n)
+		if (temp->prev->n > temp->n)
 		{
 			temp2 = temp;
-			while (temp2->prev->n > temp2->n)
-			{
+			temp = temp->prev;
+			do {
 				temp2->prev->next = temp2->next;
 				temp2->next->prev = temp2->prev;
-				temp2->prev->prev->next = temp2;
-				temp2->prev->prev = temp2;
-				temp2->prev = temp2->prev->prev;
-			}
+				temp2->next = temp2->prev;
+				if (temp2->prev->prev != NULL)
+				{
+					temp2->prev = temp2->prev->prev;
+					temp2->prev->prev->next = temp2;
+ 					temp2->next->prev = temp2;
+				}
+				else
+				{
+/*					temp2->temp2;*/
+					temp2->prev = NULL;
+					*list = temp2;
+					}
+ 				temp2 = temp2->prev;
+				print_list(*list);
+			} while (temp2 != NULL &&
+				 temp2->prev != NULL &&
+				 temp2->prev->n > temp2->n);
 		}
+		temp = temp->next;
 	}
 }
-
