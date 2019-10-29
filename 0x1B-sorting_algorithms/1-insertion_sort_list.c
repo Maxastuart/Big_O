184002536054202ea09e934e542fac9b83f92f81
maxastuart <maxastuart@gmail.com>
2019-10-29 04:48:29 +0000

    Refactored for Readability & Fixed All Bugs, Now Only Betty Fails: Function >40 Lines

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -4,52 +4,60 @@
  * insertion_sort_list - sorts a 2x linked list of integers in ascending order
  * @list: A doubly linked list
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp2, *temp = (*list)->next;
+	listint_t *temp2, *temp;
 
-        if (list == NULL)
-                return;
-
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
+	temp = (*list)->next;
 	while (temp != NULL)
 	{
 		if (temp->prev->n > temp->n)
 		{
 			temp2 = temp;
-			temp = temp->prev;
 			while (temp2 != NULL &&
 				 temp2->prev != NULL &&
 				 temp2->prev->n > temp2->n)
 			{
-				if (temp2->next != NULL)
-				{
-					temp2->prev->next = temp2->next;
-					temp2->next->prev = temp2->prev;
+				if (temp2->next == NULL &&
+				    temp2->prev->prev == NULL)
+				{ /* 2 element list */
+					temp2->prev->next = NULL;
+					temp2->prev->prev = temp2;
 					temp2->next = temp2->prev;
+					temp2->prev = NULL;
+					*list = temp2;
 				}
-				if (temp2->next == NULL)
-				{
+				else if (temp2->next == NULL)
+				{ /* end of list */
 					temp2->prev->prev->next = temp2;
 					temp2->next = temp2->prev;
 					temp2->prev = temp2->prev->prev;
 					temp2->next->prev = temp2;
 					temp2->next->next = NULL;
 				}
-				else if (temp2->prev->prev != NULL)
-				{
+				else if (temp2->prev->prev == NULL)
+				{ /* beginning of list */
+					temp2->prev->next = temp2->next;
+					temp2->next->prev = temp2->prev;
+					temp2->next = temp2->prev;
+					temp2->prev = NULL;
+					temp2->next->prev = temp2;
+					*list = temp2;
+				}
+				else
+				{ /* mid-list, no NULL in sight */
+					temp2->prev->next = temp2->next;
+					temp2->next->prev = temp2->prev;
+					temp2->next = temp2->prev;
 					temp2->prev->prev->next = temp2;
 					temp2->prev = temp2->prev->prev;
- 					temp2->next->prev = temp2;
-				}
-				else if (temp2->prev->prev == NULL)
-				{
-					temp2->prev->next = temp2;
-					temp2->prev = NULL;
-					*list = temp2;
+					temp2->next->prev = temp2;
 				}
 				print_list(*list);
 			}
+			temp = temp2->next;
 		}
 		temp = temp->next;
 	}
