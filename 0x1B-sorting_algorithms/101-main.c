f5934dcea39ae624ac6c6376275ccbda5c1a83e6
zinczar <brendan@wineisneat.com>
2019-10-31 04:11:36 +0000

    added flag

--- 0x1B-sorting_algorithms/101-main.c
+++ 0x1B-sorting_algorithms/101-main.c
@@ -41,8 +41,8 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
 	listint_t *list;
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {2,2,2,2,2,2,2,2,1, 19, 48, 71, 99, 13, 52, 96, 73, 86, 7};
+ 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	list = create_listint(array, n);
 	if (!list)
